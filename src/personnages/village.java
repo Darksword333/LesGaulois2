276d0ed391ba6c4e930c597bc46241e3e0be5604
Gabriel MAZET <49925197+Darksword333@users.noreply.github.com>
2023-09-26 17:49:41 +0000

    <Début du TP2>

---
+++ src/personnages/village.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Village{
+    private String nom;
+    private Chef chef;
+    private int nbVillageois = 0;
+    private String[nbVillageois] villageois;
+    
+    public Village(String nom, int nbVillageois){
+        this.nom = nom;
+        this.nbVillageois = nbVillageois;
+    }
+
+    public void setChef(Chef chef){
+        this.chef = chef;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+}
